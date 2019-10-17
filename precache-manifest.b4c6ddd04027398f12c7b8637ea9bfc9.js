59c38cdb9dd13780efe9a85e5e52e8ff300b7836
Justus Soh <justus.soh@hotmail.com>
2019-10-17 14:58:04 +0800

    Updates

---
+++ precache-manifest.b4c6ddd04027398f12c7b8637ea9bfc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49912f83f92364ffd55004de9948455",
+    "url": "/BT3103-P2J/index.html"
+  },
+  {
+    "revision": "14c8fb193f90fba5544e",
+    "url": "/BT3103-P2J/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "b1980aa9f71338d113b4",
+    "url": "/BT3103-P2J/static/css/main.b45ca241.chunk.css"
+  },
+  {
+    "revision": "14c8fb193f90fba5544e",
+    "url": "/BT3103-P2J/static/js/2.5f89e082.chunk.js"
+  },
+  {
+    "revision": "b1980aa9f71338d113b4",
+    "url": "/BT3103-P2J/static/js/main.99769f11.chunk.js"
+  },
+  {
+    "revision": "25f89d5e09d1f3fa4c88",
+    "url": "/BT3103-P2J/static/js/runtime-main.432cf80b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BT3103-P2J/precache-manifest.45c297bb39a9c80d69e53805f7006c0b.js"
+  "/BT3103-P2J/precache-manifest.b4c6ddd04027398f12c7b8637ea9bfc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BT3103-P2J/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
